f72b2222dd7c28c6e56295d5fc4b956ab6187af0
jisuWoo <wjs03112@gmail.com>
2023-04-05 22:22:37 +0900

    jisuWoo,stage update

--- Lecture05/GameEngine/GameEngine.cpp
+++ Lecture05/GameEngine/GameEngine.cpp
@@ -6,7 +6,7 @@
 int print_title_screen() {
 
 
-	std::cout << "■■■■■■■■■■■■■■■■■■■■■" << std::endl;
+	std::cout << "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" << std::endl;
 	std::cout << "■                                      ■" << std::endl;
 	std::cout << "■                                      ■" << std::endl;
 	std::cout << "■             지렁이 게임              ■" << std::endl;
@@ -17,20 +17,51 @@ int print_title_screen() {
 	std::cout << "■  3. 게임 랭킹 보기                   ■" << std::endl;
 	std::cout << "■  4. 게임 종료 (esc)                  ■" << std::endl;
 	std::cout << "■                                      ■" << std::endl;
-	std::cout << "■■■■■■■■■■■■■■■■■■■■■" << std::endl;
+	std::cout << "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" << std::endl;
 
 	return 0;
 
 }
 
+void gotoxy(int x, int y) {
+	//x, y 좌표 설정
+	COORD pos = { x,y };
+	//커서 이동
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+int print_game_screen(int stage_width, int stage_height)
+{
+
+	for (int i = 0; i < stage_width; i++) {
+		gotoxy(i, 0);
+		std::cout << "■" << std::endl;
+	}
+	for (int i = 0; i < stage_height; i++) {
+		gotoxy(0, i);
+		std::cout << "■" << std::endl;
+	}
+	for (int i = 0; i < stage_height; i++) {
+		gotoxy(stage_width, i);
+		std::cout << "■" << std::endl;
+	}
+	for (int i = 0; i <= stage_width; i++) {
+		gotoxy(i, stage_height);
+		std::cout << "■" << std::endl;
+	}
+
+	return 0;
+}
+
+
 //game state == 2
 int print_introduction_screen() {
 
-	std::cout << "■■■■■■■■■■■■■■■■■■■■■" << std::endl;
-	std::cout << "■          게임 설명화면입니다.        ■" << std::endl;
-	std::cout << "■ Snake bite란... 인터넷에...          ■" << std::endl;
-	std::cout << "■■■■■■■■■■■■■■■■■■■■■" << std::endl;
-	std::cout << "■   타이틀화면으로 돌아갈까요? (Y/N)   ■" << std::endl;
+	std::cout << "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" << std::endl;
+	std::cout << "■          게임 설명화면입니다.      ■" << std::endl;
+	std::cout << "■ Snake bite란... 인터넷에...        ■" << std::endl;
+	std::cout << "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" << std::endl;
+	std::cout << "■   타이틀화면으로 돌아갈까요? (Y/N) ■" << std::endl;
 
 	return 0;
 }
@@ -58,11 +89,12 @@ int main() {
 				switch (key_input)
 				{
 					case '1':
-						//game_state = 1;
+						game_state = 1;
+						mod_refresh = 1;
 						break;
 					case '2':
-						mod_refresh = 1;
 						game_state = 2;
+						mod_refresh = 1;
 						break;
 					case '3':
 						break;
@@ -76,7 +108,19 @@ int main() {
 				}
 				break;
 			case 1:
-
+				//system("cls");
+				if (mod_refresh) {
+					int x=0;
+					int y=0;
+					std::cout << "■맵을 만들기 위해 x,y값을 입력해주세요!■" << std::endl;
+					std::cout << "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" << std::endl;
+					scanf_s("%d", &x);
+					scanf_s("%d", &y);
+					system("cls");
+					print_game_screen(x, y);
+				};
+				key_input = _getch();
+				mod_refresh = 0;
 				break;
 			case 2:
 
@@ -102,6 +146,5 @@ int main() {
 		}
 		
 	}
-
 	return 0;
 }
