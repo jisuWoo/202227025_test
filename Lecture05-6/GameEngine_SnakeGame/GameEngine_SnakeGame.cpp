c06000b8d2c046a5c9c306f1cbe00b717266a334
jisuWoo <127714602+jisuWoo@users.noreply.github.com>
2023-06-04 17:44:14 +0900

    Update GameEngine_SnakeGame.cpp

--- Lecture05-6/GameEngine_SnakeGame/GameEngine_SnakeGame.cpp
+++ Lecture05-6/GameEngine_SnakeGame/GameEngine_SnakeGame.cpp
@@ -1,14 +1,32 @@
-﻿#include <iostream>
+#include <iostream>
 #include <conio.h>
 #include <Windows.h>
+
+#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define DOWN 80
+#define PAUSE 112
+#define ESC 27
 
 //game state == 0
 void print_game_screen(char** map, int stage_width, int stage_height);
 
+int x[100], y[100]; //x,y 좌표값을 저장 총 100개 
+int food_x, food_y; //food의 좌표값을 저장 
+int length; //몸길이를 기억 
+int speed; //게임 속도 
+int score; //점수 저장  --reset함수에 의해 초기화됨
+int best_score=0; //최고 점수 저장 --reset함수에 의해 초기화 되지 않음 
+int last_score=0; //마지막 점수 저장  --reset함수에 의해 초기화 되지 않음
+int dir; //이동방향 저장 
+int key; //입력받은 키 저장 
+
+
 int print_title_screen() {
 
 
-	std::cout << "■■■■■■■■■■■■■■■■■■■■■" << std::endl;
+	std::cout << "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" << std::endl;
 	std::cout << "■                                      ■" << std::endl;
 	std::cout << "■                                      ■" << std::endl;
 	std::cout << "■             지렁이 게임              ■" << std::endl;
@@ -19,7 +37,7 @@ int print_title_screen() {
 	std::cout << "■  3. 게임 랭킹 보기                   ■" << std::endl;
 	std::cout << "■  4. 게임 종료 (esc)                  ■" << std::endl;
 	std::cout << "■                                      ■" << std::endl;
-	std::cout << "■■■■■■■■■■■■■■■■■■■■■" << std::endl;
+	std::cout << "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" << std::endl;
 
 	return 0;
 
@@ -77,6 +95,41 @@ int print_introduction_screen() {
 	std::cout << "■   타이틀화면으로 돌아갈까요? (Y/N) ■" << std::endl;
 
 	return 0;
+}
+
+void move(int dir){
+    int i;
+ 
+    if(x[0]==food_x&&y[0]==food_y){ //food와 충돌했을 경우 
+        score+=10; //점수 증가 
+        food(); //새로운 food 추가 
+        length++; //길이증가 
+        x[length-1]=x[length-2]; //새로만든 몸통에 값 입력 
+        y[length-1]=y[length-2];
+    }
+    if(x[0]==0||x[0]==MAP_WIDTH-1||y[0]==0||y[0]==MAP_HEIGHT-1){ //벽과 충돌했을 경우 
+        game_over();
+        return; //game_over에서 게임을 다시 시작하게 되면 여기서부터 반복되므로 
+                //return을 사용하여 move의 나머지 부분이 실행되지 않도록 합니다. 
+    }
+    for(i=1;i<length;i++){ //자기몸과 충돌했는지 검사 
+        if(x[0]==x[i]&&y[0]==y[i]){
+            game_over();
+            return;
+        }
+    }
+    
+    gotoxy(MAP_X+x[length-1],MAP_Y+y[length-1],"  "); //몸통 마지막을 지움 
+    for(i=length-1;i>0;i--){ //몸통좌표를 한칸씩 옮김 
+        x[i]=x[i-1];
+        y[i]=y[i-1];
+    }
+    gotoxy(MAP_X+x[0],MAP_Y+y[0],"ㅇ"); //머리가 있던곳을 몸통으로 고침 
+    if(dir==LEFT) --x[0]; //방향에 따라 새로운 머리좌표(x[0],y[0])값을 변경 
+    if(dir==RIGHT) ++x[0];
+    if(dir==UP) --y[0]; 
+    if(dir==DOWN) ++y[0];     
+    gotoxy(MAP_X+x[i],MAP_Y+y[i],"ㅎ"); //새로운 머리좌표값에 머리를 그림 
 }
 
 
@@ -151,6 +204,29 @@ int main() {
 					scanf_s("%d", &stage_height);
 					system("cls");
 					InitMap(stage_width, stage_height);
+					switch(key)
+					{ //입력받은 키를 파악하고 실행  
+                        case LEFT:
+                        case RIGHT:
+                        case UP:
+                        case DOWN:
+                            if((dir==LEFT&&key!=RIGHT)||(dir==RIGHT&&key!=LEFT)||(dir==UP&&key!=DOWN)||
+                            (dir==DOWN&&key!=UP))//180회전이동을 방지하기 위해 필요. 
+                            dir=key;
+                            key=0; // 키값을 저장하는 함수를 reset 
+                            break;
+                        case PAUSE: // P키를 누르면 일시정지 
+                            pause();
+                            break;
+                        case 115: //S키를 누르면 개발자용 status를 활성화 시킴  
+                            if(status_on==0) status_on=1;
+                            else status_on=0;
+                            key=0;
+                            break;
+                        case ESC: //ESC키를 누르면 프로그램 종료 
+                            exit(0);
+                    }
+                    move(dir); 
 				}
 				mod_refresh = 0;
 			};
