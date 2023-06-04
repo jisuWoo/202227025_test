adcd1dc5729165a8cd79b03dc291edc4a8c58c42
jisuWoo <127714602+jisuWoo@users.noreply.github.com>
2023-06-05 08:39:54 +0900

    Update GameEngine_SnakeGame.cpp

--- Lecture05-6/GameEngine_SnakeGame/GameEngine_SnakeGame.cpp
+++ Lecture05-6/GameEngine_SnakeGame/GameEngine_SnakeGame.cpp
@@ -9,19 +9,18 @@
 #define PAUSE 112
 #define ESC 27
 
+
+int length;
+int item_x;
+int item_y;
+int score;
+int x[40];
+int y[40];
+int dir;
+
 //game state == 0
 void print_game_screen(char** map, int stage_width, int stage_height);
-
-int x[100], y[100]; //x,y 좌표값을 저장 총 100개 
-int food_x, food_y; //food의 좌표값을 저장 
-int length; //몸길이를 기억 
-int speed; //게임 속도 
-int score; //점수 저장  --reset함수에 의해 초기화됨
-int best_score=0; //최고 점수 저장 --reset함수에 의해 초기화 되지 않음 
-int last_score=0; //마지막 점수 저장  --reset함수에 의해 초기화 되지 않음
-int dir; //이동방향 저장 
-int key; //입력받은 키 저장 
-
+void item(int stage_width, int stage_height);
 
 int print_title_screen() {
 
@@ -52,7 +51,7 @@ void gotoxy(int x, int y) {
 
 //game_state == 1
 int InitMap(int stage_width, int stage_height) {
-	
+
 	char** map = (char**)malloc(sizeof(char*) * (stage_height + 2));
 	for (int i = 0; i < stage_height + 2; i++) {
 		map[i] = (char*)malloc(sizeof(char) * (stage_width + 2));
@@ -88,48 +87,92 @@ void print_game_screen(char** map, int stage_width, int stage_height)
 //game state == 2
 int print_introduction_screen() {
 
-	std::cout << "■■■■■■■■■■■■■■■■■■■■" << std::endl;
+	std::cout << "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" << std::endl;
 	std::cout << "■          게임 설명화면입니다.      ■" << std::endl;
 	std::cout << "■ Snake bite란... 인터넷에...        ■" << std::endl;
-	std::cout << "■■■■■■■■■■■■■■■■■■■■" << std::endl;
+	std::cout << "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" << std::endl;
 	std::cout << "■   타이틀화면으로 돌아갈까요? (Y/N) ■" << std::endl;
 
 	return 0;
 }
 
-void move(int dir){
-    int i;
- 
-    if(x[0]==food_x&&y[0]==food_y){ //food와 충돌했을 경우 
-        score+=10; //점수 증가 
-        food(); //새로운 food 추가 
-        length++; //길이증가 
-        x[length-1]=x[length-2]; //새로만든 몸통에 값 입력 
-        y[length-1]=y[length-2];
-    }
-    if(x[0]==0||x[0]==MAP_WIDTH-1||y[0]==0||y[0]==MAP_HEIGHT-1){ //벽과 충돌했을 경우 
-        game_over();
-        return; //game_over에서 게임을 다시 시작하게 되면 여기서부터 반복되므로 
-                //return을 사용하여 move의 나머지 부분이 실행되지 않도록 합니다. 
-    }
-    for(i=1;i<length;i++){ //자기몸과 충돌했는지 검사 
-        if(x[0]==x[i]&&y[0]==y[i]){
-            game_over();
-            return;
-        }
-    }
-    
-    gotoxy(MAP_X+x[length-1],MAP_Y+y[length-1],"  "); //몸통 마지막을 지움 
-    for(i=length-1;i>0;i--){ //몸통좌표를 한칸씩 옮김 
-        x[i]=x[i-1];
-        y[i]=y[i-1];
-    }
-    gotoxy(MAP_X+x[0],MAP_Y+y[0],"ㅇ"); //머리가 있던곳을 몸통으로 고침 
-    if(dir==LEFT) --x[0]; //방향에 따라 새로운 머리좌표(x[0],y[0])값을 변경 
-    if(dir==RIGHT) ++x[0];
-    if(dir==UP) --y[0]; 
-    if(dir==DOWN) ++y[0];     
-    gotoxy(MAP_X+x[i],MAP_Y+y[i],"ㅎ"); //새로운 머리좌표값에 머리를 그림 
+int GameOver() {
+
+	std::cout << "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" << std::endl;
+	std::cout << "■        게임 오버                  ■" << std::endl;
+	std::cout << "■ Snake bite란... 인터넷에...        ■" << std::endl;
+	std::cout << "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" << std::endl;
+	std::cout << "■   타이틀화면으로 돌아갈까요? (Y/N) ■" << std::endl;
+
+	return 0;
+}
+
+void move(int dir,int stage_width, int stage_height) {
+	int i;
+
+	if (x[0] == item_x && y[0] == item_y) { //item과 충돌했을 경우
+		score += 10;
+		item(stage_width, stage_height); //새로운 food 추가
+		length++;
+		x[length] = x[length - 1]; //움직일수록 좌표값이 변해야하기 때문에
+		//전에 있던 몸통좌표값을 새로 움직인 좌표값에 넣어줌 
+		y[length] = y[length - 1];
+	}
+	if (x[0] == 3 || x[0] == stage_width - 1 || y[0] == 3 || y[0] == stage_height - 1) {
+		//gameOver();
+		return; //gameOver에서 게임을 다시 시작하게 되면 여기서부터 반복되므로 초기화
+	}
+
+	gotoxy(x[length - 1], y[length - 1]);
+	printf(" ");
+	//뱀의 좌표가 움직일때마다 맨뒤의 값을 지워줘야함 -> 공백처리
+	for (i = length - 1; i > 0; i--) {
+		x[i] = x[i - 1];
+		y[i] = y[i - 1];
+	}
+
+	gotoxy(x[0], y[0]);
+	printf("o");
+	if (dir == LEFT)
+		--x[0];
+	if (dir == RIGHT)
+		++x[0];
+	if (dir == UP)
+		--y[0];
+	if (dir == DOWN)
+		++y[0];
+
+	gotoxy(x[i], y[i]);
+	printf("◎");
+};
+
+void reset() {
+	system("cls");
+}
+
+void item(int stage_width, int stage_height) {
+	int r = 0;
+	int item_crush = 0;
+	//43, 28
+	while (1) {
+		srand((unsigned)time(NULL) + r); //계속값이 바뀔수있도록 +r을해줌
+		item_x = (rand() % stage_width - 2) + 1; //1~42까지
+		item_y = (rand() % stage_height - 2) + 1; //1~28까지
+
+		for (int i = 0; i < length; i++) {
+			if (item_x == x[i] && item_y == y[i]) {
+				item_crush = 1;
+				r++;
+				break;
+			}
+		}
+
+		if (item_crush == 1)
+			continue;
+		gotoxy(item_x, item_y);
+		printf("@");
+		break;
+	}
 }
 
 
@@ -155,7 +198,7 @@ int main() {
 			game_state = 0;
 			break;
 		case 1:
-			while (sub_title_state) 
+			while (sub_title_state)
 			{
 				if (mod_refresh)
 				{
@@ -197,38 +240,33 @@ int main() {
 			if (sub_title_state == 0 && game_state == 1) {
 				if (mod_refresh)
 				{
-					int stage_width, stage_height;
-					std::cout << "■맵을 만들기 위해 x,y값을 입력해주세요!■" << std::endl;
-					std::cout << "■■■■■■■■■■■■■■■■■■■■■" << std::endl;
-					scanf_s("%d", &stage_width);
-					scanf_s("%d", &stage_height);
-					system("cls");
-					InitMap(stage_width, stage_height);
-					switch(key)
-					{ //입력받은 키를 파악하고 실행  
-                        case LEFT:
-                        case RIGHT:
-                        case UP:
-                        case DOWN:
-                            if((dir==LEFT&&key!=RIGHT)||(dir==RIGHT&&key!=LEFT)||(dir==UP&&key!=DOWN)||
-                            (dir==DOWN&&key!=UP))//180회전이동을 방지하기 위해 필요. 
-                            dir=key;
-                            key=0; // 키값을 저장하는 함수를 reset 
-                            break;
-                        case PAUSE: // P키를 누르면 일시정지 
-                            pause();
-                            break;
-                        case 115: //S키를 누르면 개발자용 status를 활성화 시킴  
-                            if(status_on==0) status_on=1;
-                            else status_on=0;
-                            key=0;
-                            break;
-                        case ESC: //ESC키를 누르면 프로그램 종료 
-                            exit(0);
-                    }
-                    move(dir); 
+					int mapbuild = 0;
+					if (mapbuild) {
+						int stage_width, stage_height;
+						std::cout << "■맵을 만들기 위해 x,y값을 입력해주세요!■" << std::endl;
+						std::cout << "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" << std::endl;
+						scanf_s("%d", &stage_width);
+						scanf_s("%d", &stage_height);
+						system("cls");
+						InitMap(stage_width, stage_height);
+					}
+					char key = _getch();
+					switch (key) {
+					case LEFT:
+					case RIGHT:
+					case UP:
+					case DOWN:
+						if ((dir == LEFT && key != RIGHT) || (dir == RIGHT && key != LEFT) || (dir == UP && key != DOWN) || (dir == DOWN && key != UP))
+							dir = key;
+						key = 0;
+						break;
+					case ESC:
+						exit(0);
+					}
+					move(dir,stage_width, stage_height);
+
 				}
-				mod_refresh = 0;
+				//mod_refresh = 0;
 			};
 			break;
 		case 2:
